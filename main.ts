36b4158ec625570cf3099ebc75e3861024171991
antoniotecnologia <antoniotecnologia@iesperezpastor.com>
2025-12-28 11:52:58 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,25 @@
-let id_card = 0
+/**
+ * id llave azul 39974732264
+ */
+/**
+ * id tarjeta blanca
+ * 
+ * 455378732505
+ */
 I2C_LCD1602.LcdInit(39)
 MFRC522.Init()
 I2C_LCD1602.ShowString("Scan tu card", 0, 0)
 basic.forever(function () {
-    id_card = MFRC522.getID()
-    if (true) {
+    I2C_LCD1602.ShowString("Scan tu card", 0, 0)
+    if (MFRC522.getID() == 39974732264) {
         I2C_LCD1602.clear()
-        I2C_LCD1602.ShowString("Pasa tu tarjeta", 0, 0)
-        I2C_LCD1602.ShowNumber(MFRC522.getID(), 0, 1)
-        basic.pause(3000)
+        I2C_LCD1602.ShowString("pasa", 0, 0)
+        basic.showIcon(IconNames.Yes)
+        basic.pause(2000)
+    } else {
         I2C_LCD1602.clear()
-        I2C_LCD1602.ShowString("Pon la tarjeta", 0, 0)
+        I2C_LCD1602.ShowString("stop", 0, 0)
+        basic.showIcon(IconNames.No)
+        basic.pause(2000)
     }
 })
